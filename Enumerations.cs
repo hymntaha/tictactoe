4d1a4b7c2eb003d3766978a970f2d548413f6e54
taha <tahauygun@gmail.com>
2017-06-06 21:22:39 -0500

    Enum integrate

---
+++ Enumerations.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Author: Taha Uygun
+ * Mail Control TicTacToe
+ */
+
+namespace TicTacToe
+{
+    public enum Team
+    {
+        Undetermined,
+        X,
+        O
+    }
+
+    public enum OpponentMoveMode
+    {
+        Random,
+        Logical
+    }
+}
